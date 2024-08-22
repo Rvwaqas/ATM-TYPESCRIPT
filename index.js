c0546b37b185c8f9cc6900b07c7106b992afd57c
Rv waqas <145973183+Rvwaqas@users.noreply.github.com>
2024-08-22 23:30:20 +0500

    Add files via upload
    add

---
+++ index.js
@@ -0,0 +1,64 @@
+import inquirer from 'inquirer';
+import { faker } from '@faker-js/faker';
+const userInfo = () => {
+    let users = [];
+    for (let i = 0; i < 5; i++) {
+        let user = {
+            id: i,
+            pin: 1000 + i,
+            name: faker.person.fullName(),
+            accountNumber: Math.floor(Math.random() * 30000),
+            balance: 10000 + i
+        };
+        users.push(user);
+    }
+    return users;
+};
+//atm machine
+const atmMachine = async (users) => {
+    let response = await inquirer.prompt({
+        type: 'number',
+        message: "Enter your pin",
+        name: "pin"
+    });
+    const user = users.find((val) => val.pin == response.pin);
+    if (user) {
+        console.log("Welcome Account Holder");
+        atmFunc(user);
+        return;
+    }
+    else {
+        console.log("Invalid user");
+    }
+};
+// atm funtion
+const atmFunc = async (user) => {
+    const ans = await inquirer.prompt({
+        type: 'list',
+        name: "select",
+        choices: ["withdraw", "balance inquirer", "Exit"]
+    });
+    if (ans.select == "withdraw") {
+        const amount = await inquirer.prompt({
+            type: "number",
+            name: "ruppee",
+            message: "Enter your Amount"
+        });
+        if (amount.ruppee > user.balance) {
+            console.log("Not enough balance");
+        }
+        if (amount.ruppee > 25000) {
+            console.log("you cannot enter amount greater then 250000");
+        }
+        console.log(`cash withdraw successful ${amount.ruppee}`);
+        console.log(`available balance is ${user.balance - amount.ruppee}`);
+    }
+    if (ans.select == "balance inquirer") {
+        console.log(`Your balance is ${user.balance}`);
+    }
+    if (ans.select == "Exit") {
+        console.log(`Thank you `);
+    }
+};
+let user = userInfo();
+atmMachine(user);
